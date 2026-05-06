b0e2173b5dcfe8bd86bdb4cb61d5e64db89d51a2
LUCAS INÁCIO ROCHA DE OLIVEIRA <lucas.oliveira36@portalsesisp.org.br>
2026-05-06 20:08:55 -0300

    Projeto CNPJ com API

---
+++ pp_aula03_ex01_api.py
@@ -0,0 +1,16 @@
+# Autor: Lucas Inácio
+# API - CNPJ
+
+# Importar biblioteca
+import requests
+
+cnpj = "05061448000139"
+url = f"https://api.opencnpj.org/{cnpj}"
+res = requests.get(url)
+
+dados = res.json()
+
+print(f'CNPJ: {dados["cnpj"]}')
+print("Razão Social: ", dados['razao_social'])
+print(f'Nome Fantasia: {dados["nome_fantasia"]}')
+print(f'Situação Cadastral: {dados["situacao_cadastral"]}')
